12ef791a27afb253be6e504df97e298f69dc7173
Railson Rames <rames.cloud@outlook.com.br>
2019-04-02 15:49:10 -0300

    Funcionando

---
+++ TestandoTemplateAdminLTE/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestandoTemplateAdminLTE.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class DashboardController : Controller
+    {
+        [Route("")]
+        [Route("admin")]
+        [Route("index")]
+        [Route("admin/dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("admin/dashboard2")]
+        public IActionResult Dashboard2()
+        {
+            return View();
+        }
+    }
+}
